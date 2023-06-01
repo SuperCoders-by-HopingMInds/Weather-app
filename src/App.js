dd7567ec68d50a0f235337e5fd588435526aba26
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-01 17:54:40 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [user_latitude, setUserLatitude] = useState(0);
+    const [user_longitude, setUserLongitude] = useState(0);
+    const [hemisphere, setHemisphere] = useState("");
+    const [month, setMonth] = useState("");
+
+    function getLocation(){
+         if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition(
+                    (position) =>{
+                        setUserLatitude(position.coords.latitude);
+                        setUserLongitude(position.coords.longitude);
+                        console.log("Hello")
+                        if(position.coords.latitude>0){
+                            setHemisphere(value => "Northern Hemisphere");
+                        }
+                        else if(position.coords.latitude<0){
+                            setHemisphere(value => "Southern Hemisphere");
+                        }
+                        else{
+                            setHemisphere(value => "Equator");
+                        }
+                    }
+                );
+         }
+        let all_months =  ["January","February","March","April","May","June","July","August","September","October","November","December"]
+        let date = new Date();
+        setMonth( all_months[date.getMonth()])
+
+    }
+    
+    return(
+        <div>
+            <h1>React Geolocation</h1>
+            <p>Latitude: {user_latitude}</p>
+            <p>Longitude: {user_longitude}</p>
+            <p>{hemisphere}</p>
+            <p>{month}</p>
+            <button onClick={getLocation}>Get Location</button>
+        </div>
+    )
 }
 
 export default App;
+
+
+// function hello(e){
+//     e.preventDefault();
+
+// }
